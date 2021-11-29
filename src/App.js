44d574b25f649ca39420b1ea558f14da2955ed6f
d33kshant <d33kshant@gmail.com>
2021-11-30 00:35:09 +0530

    Added Basic Authantication

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
-const App = ()=>{
-  return "Hello World"
+import Login from './screens/Login'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { getAuth } from 'firebase/auth'
+import app from './firebase'
+import Loading from './screens/Loading'
+
+const App = () => {
+
+	const [user, loading] = useAuthState(getAuth(app))
+
+	if (loading) return <Loading />
+	if (user) {
+		console.log(user)
+		return `User: ${user.uid}`
+	}
+
+	return (
+		<Login />
+	)
 }
 
 export default App
