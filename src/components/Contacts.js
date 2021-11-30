4ba29d8b707db9c570c04acde1316384d1c97fa0
d33kshant <d33kshant@gmail.com>
2021-11-30 12:25:08 +0530

    Added Contacts Component

---
+++ src/components/Contacts.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+	transition: transform ease 0.3s;
+	width: 100%;
+	height: 100%;
+	flex-shrink: 0;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+`
+
+const Contacts = ({children, ...props}) => {
+	return (
+		<Container {...props} >
+			{children}
+		</Container>
+	)
+}
+
+export default Contacts
