7da064858dc5bfa9d3e0b3c85a67cfb917efac0a
d33kshant <d33kshant@gmail.com>
2021-12-02 01:03:56 +0530

    Added Input Component

---
+++ src/components/Input.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import styled from 'styled-components'
+import IconButton from './IconButton'
+
+const Container = styled.div`
+	border-top: 1px solid lightgray;
+	/* border-radius: 32px; */
+	width: 100%;
+	position: sticky;
+	box-sizing: border-box;
+	display: flex;
+	background: white;
+	bottom: 0;
+	padding: 12px 0;
+`
+
+const InputBox = styled.input`
+	flex: 1;
+	border: none;
+	outline: none;
+	padding: 0 16px;
+	font-size: 14px;
+	font-family: inherit;
+`
+
+const SendButton = styled(IconButton)`
+	:hover {
+		svg {
+			stroke: #0095f6;
+		}
+	}
+`
+
+const Input = ({ onSubmit, to, ...props}) => {
+	return (
+		<Container >
+			<InputBox ref={to} {...props} placeholder="Type something here" />
+			<SendButton onClick={onSubmit}>
+				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" ><line x1="22" y1="2" x2="11" y2="13"/><polygon points="22 2 15 22 11 13 2 9 22 2"/></svg>
+			</SendButton>
+		</Container>
+	)
+}
+
+export default Input
