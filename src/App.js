bed3bbe3a5c8118cad098901623ead886938787a
d33kshant <d33kshant@gmail.com>
2021-12-02 01:09:00 +0530

    Updated App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import Login from './screens/Login'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { getAuth } from 'firebase/auth'
+import { getFirestore, setDoc, doc, serverTimestamp} from 'firebase/firestore'
 import app from './firebase'
 import Loading from './screens/Loading'
 import Home from './screens/Home'
+import { useEffect } from 'react'
 
 const App = () => {
 
 	const [user, loading] = useAuthState(getAuth(app))
 
-	if (loading) return <Loading />
-	if (user) {
-		console.log(user)
-		return <Home user={user} />
-	}
+	useEffect(() => {
+		if(user) {
+			setDoc(doc(getFirestore(app), 'users', user.uid), {
+				name: user.displayName,
+				email: user.email,
+				avatar: user.photoURL,
+				uid: user.uid,
+				lastSeen: serverTimestamp()
+			}, {merge: true}).catch(()=>{})
+		}
+	}, [user])
 
-	return (
-		<Login />
-	)
+	if (loading) return <Loading />
+	if (user) return <Home user={user} />
+
+	return <Login />
 }
 
 export default App
