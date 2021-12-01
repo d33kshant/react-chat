c429668bd4d4f727e76b7623865a84c5db99412c
d33kshant <d33kshant@gmail.com>
2021-12-02 01:09:23 +0530

    Added Chat Component

---
+++ src/components/Chat.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+	width: fit-content;
+	background: lightgray;
+	border: 1px solid lightgray;
+	box-sizing: border-box;
+	border-radius: 8px;
+	max-width: 50%;
+	padding: 8px;
+`
+
+const Text = styled.p`
+	margin: 0;
+`
+
+const Time = styled.p`
+	color: gray;
+	font-size: 14px;
+	margin: 0;
+	margin-top: 8px;
+`
+
+
+const Chat = ({self, time, children}) => {
+	return (
+		<Container style={self? {marginLeft: 'auto'} : {marginRight: 'auto', background: 'white'}} >
+			<Text>
+				{children}
+			</Text>
+			<Time>{time}</Time>
+		</Container>
+	)
+}
+
+export default Chat
