a77b99722a98dc37ca842946294bbaf76eefdd54
d33kshant <d33kshant@gmail.com>
2021-12-02 01:03:29 +0530

    Added Title Component

---
+++ src/components/Title.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const Title = styled.p`
+	margin: 0;
+	padding: 0;
+	font-family: inherit;
+	font-weight: 600;
+	font-size: 16px;
+`
+
+export default Title
