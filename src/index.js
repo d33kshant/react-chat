f7b097e35fd531af70dd566317d653eb361b3327
d33kshant <d33kshant@gmail.com>
2021-12-02 15:17:17 +0530

    Wrapped App Component

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-ReactDOM.render(<App />, document.getElementById('root'))
+import AppWrapper from './components/AppWrapper';
+ReactDOM.render(<AppWrapper><App /></AppWrapper>, document.getElementById('root'))
