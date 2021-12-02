26b56fe927589c0f3554034e80467e26c7716eec
d33kshant <d33kshant@gmail.com>
2021-12-02 15:15:08 +0530

    Updated Styled & Added Auto Scroll To Bottom

--- src/components/ChatList.js
+++ src/components/ChatList.js
@@ -1,5 +1,5 @@
 import { collection, getFirestore, orderBy, query } from '@firebase/firestore'
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useCollection } from 'react-firebase-hooks/firestore'
 import styled from 'styled-components'
 import app from '../firebase'
@@ -7,28 +7,37 @@ import Chat from './Chat'
 
 const Container = styled.div`
 	width: 100%;
-	height: 100%;
-	overflow-y: scroll;
+	height: calc(100% - 100px);
+	overflow-y: auto;
 	display: flex;
-	flex-direction: column;
+	flex-direction: column-reverse;
 	align-items: center;
-	justify-content: flex-end;
+	/* justify-content: flex-end; */
 	box-sizing: border-box;
-	padding: 0 16px;
+	padding: 0 8px;
 	gap: 8px;
+	flex: 1;
 `
 
 const Space = styled.div`
 	width: 100%;
-	height: 8px;
+	/* height: 8px; */
 `
 
 const ChatList = ({ room, self }) => {
 
-	const [chats, loading, error] = useCollection(query(collection(getFirestore(app), 'rooms', room, 'chats'), orderBy('time')))
+	const [chats, loading, error] = useCollection(query(collection(getFirestore(app), 'rooms', room, 'chats'), orderBy('time', 'desc')))
+	const endRef = useRef()
+
+	useEffect(()=>{
+		if(chats){
+			endRef.current.scrollIntoView({ behavior: "smooth" })
+		}
+	}, [chats])
 
 	return (
 		<Container>
+			<Space ref={endRef}>{/* Just for some extra bottom spacing */}</Space>
 			{
 				error ? 'Ooops Something Went Wrong' :
 				loading ? 
@@ -39,7 +48,7 @@ const ChatList = ({ room, self }) => {
 					return <Chat key={index} self={author===self} time={time ? time.toDate().toDateString(): 'sending..'}>{body}</Chat>
 				}) : 'Ooops'
 			}
-			<Space>{/* Just for some extra bottom spacing */}</Space>
+			<Space >{/* Just for some extra bottom spacing */}</Space>
 		</Container>
 	)
 }
