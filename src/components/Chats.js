420ecba39841b02bd94d7cce9f6e5f167840da88
d33kshant <d33kshant@gmail.com>
2021-12-03 17:58:50 +0530

    Enabled Transition

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
-	/* transition: transform ease 0.3s; */
+	transition: transform ease 0.3s;
 	width: 100%;
 	height: 100%;
 	flex-shrink: 0;
